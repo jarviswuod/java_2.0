e765227a2b2c20426d40704b730ea761fdd320aa
jarviswuod <johnsonmakings@gmail.com>
2025-06-27 23:32:47 +0300

    Begun relearning java fresh

---
+++ java concepts/conditionalOperators.java
@@ -0,0 +1,11 @@
+public class conditionalOperators {
+    public static void main(String[] args) {
+        int age = 37;
+
+        if (age > 18) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are a child");
+        }
+    }
+}
---
+++ java concepts/typeCastingAndConversion.java
@@ -0,0 +1,20 @@
+public class typeCastingAndConversion {
+    public static void main(String args[]) {
+
+        // CONVERSION - Smaller Valued variable to a Big valued Variable
+        // Also called implicit type casting
+
+        float points = 15.4f;
+        double morePoints = points;
+        System.out.println("MorePoints: " + morePoints);
+
+        int ageYears = 22;
+        double ageMonths = ageYears * 12;
+        System.out.println("ageMonths: " + ageMonths);
+
+        // CASTING - Bigger value variable to a small Value Variable
+        double ageInMonths = 20;
+        int ageInYears = (int) ageInMonths / 12;
+        System.out.println("ageInYears: " + ageInYears);
+    }
+}
