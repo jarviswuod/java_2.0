5fc53f8fdabfb31a8de0024f9cda070acd7cf161
jarviswuod <johnsonmakings@gmail.com>
2025-06-29 22:54:29 +0300

    Tenary and Logical operators covered

--- 01. java concepts/logicalOperators.java
+++ 01. java concepts/logicalOperators.java
@@ -2,5 +2,14 @@ public class logicalOperators {
 
     public static void main(String args[]) {
         // && || !
+        int age = 20;
+        boolean hasLicense = true;
+
+        if (age >= 18 && hasLicense == true) {
+            System.out.println("You are allowed to drive");
+        } else {
+            System.out.println("You are not allowed to drive");
+        }
+
     }
 }
---
+++ 01. java concepts/tenaryOperator.java
@@ -0,0 +1,10 @@
+public class tenaryOperator {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+        String results = age >= 18 ? "You are an adult" : "You are still a kid";
+
+        System.out.println(results);
+    }
+}
