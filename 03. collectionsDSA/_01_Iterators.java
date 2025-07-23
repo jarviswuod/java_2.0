4c6753b43a0273da2d9f1361346a2e40463f50df
jarviswuod <johnsonmakings@gmail.com>
2025-07-23 22:45:43 +0300

    Started learning collections

---
+++ 03. collectionsDSA/_01_Iterators.java
@@ -0,0 +1,28 @@
+public class _01_Iterators {
+    public static void main(String[] args) {
+
+        OurGenericList<Integer> list = new OurGenericList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+    }
+}
+
+class OurGenericList<T> {
+    private T[] items;
+    private int size;
+
+    public OurGenericList() {
+        size = 0;
+        items = (T[]) new Object[100];
+    }
+
+    public void add(T item) {
+        items[size++] = item;
+    }
+
+    public T getItemAtIndex(int index) {
+        return items[index];
+    }
+}
