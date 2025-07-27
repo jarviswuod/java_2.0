21ef154073d5360edc7122841526e53218eb1dd4
jarviswuod <johnsonmakings@gmail.com>
2025-07-27 22:38:23 +0300

    Collections revision

---
+++ 03. collectionsDSA/_01_CollectionsIntro.java
@@ -0,0 +1,19 @@
+/*
+
+    NOTES:
+    - Collections:
+         - A collection allows a group of objects to be treated as a sungle unit
+         - The Java Collections Framework provides 3 main parts:
+             - The core interfaces
+             - Concrete classes(Implementation of core interfaces)
+             - Static utility methods to perform operations as sorting and searching, or creating collections and arrays
+
+
+ */
+
+public class _01_CollectionsIntro {
+
+    public static void main(String[] args) {
+
+    }
+}
