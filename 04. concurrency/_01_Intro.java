a29de5299967810f0a681e4536c659a64880ff53
jarviswuod <johnsonmakings@gmail.com>
2025-08-15 22:46:54 +0300

    Learning Multithreading and Concurrency

---
+++ 04. concurrency/_01_Intro.java
@@ -0,0 +1,29 @@
+/*
+ 
+    NOTES:
+    - Multitasking
+        - Multitasking refers to handling multiple tasks at the same time, can be achieved in computers too.
+
+        - Types of multitasking:
+            - Process-based multitasking:
+                -  This refers to running multiple programs at the same time on a computer. Example:
+                 Operating 2 different apps or programs
+
+            - Thread-based multitasking:
+                - This refers to a computer program running multiple threads/parts in parallel. Example Ms Word that is printing and formatting text at the same time
+
+
+        - Threads vs Process:
+            - Threads share the same address space
+            - Context switching between threads is usually less expensice than between processes since they share same address
+            - The cost of communication between threads is realtively low because of sharing same space address between threads
+
+
+        - Why MultiThreading?
+            - In a single-threaded environment, only one task at a time can be performed, CPU cycles are wasted.
+            - Multitasking allows idle CPU time to be put to good use.
+
+ */
+
+public class _01_Intro {
+}
---
+++ 04. concurrency/_02_Threads.java
@@ -0,0 +1,4 @@
+public class _02_Threads {
+    public static void main(String[] args) {
+    }
+}
