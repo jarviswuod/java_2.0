ff73dd2ec1bada124fadc7b64f1ccd371e6a9dd6
jarviswuod <johnsonmakings@gmail.com>
2025-08-28 11:44:12 +0300

    Learning Design Patterns

---
+++ 05. design patterns/_19_InterpreterDesignPattern/Main.java
@@ -0,0 +1,25 @@
+package _19_InterpreterDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedWomanExpression();
+
+        System.out.println("John is a male : " + isMale.interpret("John"));
+        System.out.println("Julie is a married woman : " + isMarriedWoman.interpret("Julie"));
+    }
+
+    // Rule: Robert and John are male
+    public static Expression getMaleExpression() {
+        Expression robert = new TerminalExpression("Robert");
+        Expression john = new TerminalExpression("John");
+        return new OrExpression(robert, john);
+    }
+
+    // Rule: Julie is a married woman
+    public static Expression getMarriedWomanExpression() {
+        Expression julie = new TerminalExpression("Julie");
+        Expression married = new TerminalExpression("Married");
+        return new OrExpression(julie, married);
+    }
+}
---
+++ 05. design patterns/_23_StrategyDesignPattern/Main.java
@@ -0,0 +1,14 @@
+package _23_StrategyDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Context context = new Context(new AddOperation());
+        System.out.println("10 + 5 : " + context.executeStrategy(10, 5));
+
+        context = new Context(new SubtractOperation());
+        System.out.println("10 - 5 : " + context.executeStrategy(10, 5));
+
+        context = new Context(new MultiplyOperation());
+        System.out.println("10 * 5 : " + context.executeStrategy(10, 5));
+    }
+}
