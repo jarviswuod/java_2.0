c0f725782d652ca676ed69265ae4f9c612442b16
jarviswuod <johnsonmakings@gmail.com>
2025-09-08 17:16:55 +0300

    Design Patterns

--- 05. design patterns/2. patterns/_02_OOP_Principles/Notes.java
+++ 05. design patterns/2. patterns/_02_OOP_Principles/Notes.java
@@ -29,6 +29,49 @@ package _02_OOP_Principles;
         Prevention/ overcoming/ Mitigation Strategies:
             - To mitigate the Fragile Base Class problem, software developers can use design principle such as Open/ Closed Principle and dependency Inversion Principle, as well as design patterns like Composition over inheritance. These approaches promote loose coupling, encapsulation and moduler design, reducing the impact of changes in base classes
 
+    - Unified Modeling Language:UML
+        UML is a language used to model our systems/ the relationships between out classes.
+        
+        - Types of relationships:
+            - Inheritance Relationship:
+                - Represented by an arrow: The Dog inherits from, or extends, the Animal clas:
+                    public class Dog extends Animal {
+                        // Logic goes here
+                    }
+
+            - Composition Relationship:
+                - Represented with a filled diamond
+                    public class Dog {
+                        private Size size;
+                        // Rest of the logic
+                    }
+
+            - Association Relationship:
+                - Represented by an arrow
+
+
+            - Dependency Relationship: represented by a dashed arrow
+                public class Dog {
+                    public void render(Document document) {...}
+                }
+
+
+
+        - Difference between Association and Composition Relationship:
+            - Association:
+                - When a child object can still exist without it's other object class
+                    - Example:
+                        - A person has a Car, but is not composed of a Car.
+                        - A person holds a reference to Car so it can interact with it, but a Person can exist without a Car.
+
+
+            - Composition:
+                - When a child object wouldn't be able to exist without it's parent object
+                    - Example:
+                        - A hotel is composed of it's rooms, and HotelBathroom cannot exist without Hotel(destory the hotel you destroy the hotel bathroom - it can't exist by itself)
+                        - If a Customer is destroyed their SHippingCard and Orders are aso lost too, therefore Customer is composed of ShoppingCart and Orders.
+                        - If Orders are lost, OrderDetails and ShippingInfo are lost, the orders are composed of shipingInfor and OrderDetails
+
 */
 
 public class Notes {
