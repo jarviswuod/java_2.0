fed94afb59217860f50d39424f9684cd150de8c1
jarviswuod <johnsonmakings@gmail.com>
2025-10-10 23:40:49 +0300

    Generics Revision Season 4

---
+++ java revision/03. generics revision/season 4/_01_Intro.java
@@ -0,0 +1,20 @@
+/*
+
+    NOTES:
+    - All I know about Generics:
+    - Wildcards
+    - Typecast
+    - Lower wildcards
+    - Upper wildcards
+    - extends
+    - super
+
+    - Invarianve
+    - Contravariance
+    - Bivariance
+
+ */
+
+public class _01_Intro {
+
+}
