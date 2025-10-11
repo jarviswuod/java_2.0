40f61eabaafb4d184cf35baedb839df54bc7bcc3
jarviswuod <johnsonmakings@gmail.com>
2025-10-11 22:33:51 +0300

    Generics Revision Season 4

--- java revision/03. generics revision/season 4/_01_Intro.java
+++ java revision/03. generics revision/season 4/_01_Intro.java
@@ -2,19 +2,44 @@
 
     NOTES:
     - All I know about Generics:
-    - Wildcards
-    - Typecast
-    - Lower wildcards
-    - Upper wildcards
-    - extends
-    - super
+    - Typecast:
+        - 
 
-    - Invarianve
-    - Contravariance
-    - Bivariance
+    - Wildcards
+        - Lower wildcards <? extends Type>
+        - Upper wildcards <? super Type>
+
+    - Keywords:
+        - extends
+        - super
+
+    - invariance:
+        - Only picks the excact value needed by the method
+        - Generics is invariance by default
+
+    - contravariance:
+        - Works with values greater than the one needed
+
+    - covariance:
+        - When a value is needed you can provide a more specific value and still works
+        - By default java is Covaraint
+
+    - getput PESC method
 
  */
 
 public class _01_Intro {
+    public static void main(String[] args) {
 
+    }
+
+    // public static void add(<T extends Number>) {
+
+    // }
+}
+
+class DifferentClass<T extends Number> {
+    public static void add() {
+
+    }
 }
