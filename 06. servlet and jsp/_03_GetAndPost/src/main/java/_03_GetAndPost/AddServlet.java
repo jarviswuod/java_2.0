f4f1ad23f4c2f4bfe8efbb3c00972a7e0cb9122a
jarviswuod <johnsonmakings@gmail.com>
2025-10-21 21:16:23 +0300

    Learning Servlet and JSP

---
+++ 06. servlet and jsp/_03_GetAndPost/src/main/java/_03_GetAndPost/AddServlet.java
@@ -0,0 +1,24 @@
+package _03_GetAndPost;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+
+		int k = i + j;
+		System.out.println("Result is " + k);
+
+		PrintWriter out = res.getWriter();
+		out.println("Result is " + k);
+
+	}
+}
