ce03c3d8633df0385eb5c8d37eee70d02470b309
jarviswuod <johnsonmakings@gmail.com>
2025-10-24 21:05:40 +0300

    Design Pattern Revision Season 1

---
+++ java revision/05. design patterns/_01_Notes.java
@@ -0,0 +1,106 @@
+/*
+
+    NOTES:
+    - OOP concepts
+        - Encapsulation
+            - Key:
+                - calling the deposit on the constructor
+                - Keeping the fields safe and only accessing then via public methods
+                - Logic should be kept in mind when creating the public methods
+                - Use of IllegalArgumentException
+
+        - Abstraction
+            - Hiding uncessary internal implementation from a user and only making accesible the useful ones
+            - Making everything private and only making the neccesary required public, this way when the public method is called the internal private methods are all called in accordance
+            - 
+
+        - Inheritance
+            - tight coupling -> Brittle software
+
+        - Polymorphism
+        - Composition
+            - COMPOSITION VS INHERITANCE:
+                - Fragile-Base class
+
+        - Coupling
+            - loose coupling
+            - tight coupling
+
+    - Solid Principles:
+        - Single Responsibility Principle
+        - Open/Closed Principle
+        - Liskov Substitution Principle
+        - Interface Segregation Principle
+        - Dependency Inversion Principle
+
+
+    - Design Patterns:
+        - Creational:
+            - These patterns focus on how Objects are created
+            - Types:
+                - Singleton
+                - prototype
+
+        - Structural:
+            - These patterns focus on how Objects relate and interact with each other to perform a specific purpose
+            - Types:
+                - Facade
+                - Decorator
+                - Builder
+                - flyweight
+
+        - Behavioral:
+            - These patterns focus on how Objects communicate with each other to accomplish a task
+            - Types:
+                - ChainOfResponsibility
+                - Mediator
+                - Momento
+
+
+    1. Creational
+        - AbstractFactory
+        - Builder
+        - FactoryMethod
+        - prototype
+        - Singleton
+
+        - Abs-bui-fact-prot-sing
+
+
+    2. Structural
+        - Adapter
+        - Bridge
+        - Composite
+        - Decorator
+        - Facade
+        - Flyweight
+        - Proxy
+
+        - Ada-bri-comp-dec-faca-fly-prox
+
+
+    3. Behavioral
+        - ChainOfResponsibility
+        - Command
+        - Interpreter
+        - Iterator
+        - Mediator
+        - Momento
+        - Observer
+        - State
+        - Stratergy
+        - Template
+        - Visitor
+
+        - Chai-comm-int-ite-med-mom-obs-stat-strat-temp-vis
+
+
+    - Abbreviation:
+        - Abs-bui-fact-prot-sing
+        - Ada-bri-comp-dec-faca-fly-prox
+        - Chai-comm-int-ite-med-mom-obs-stat-strat-temp-vis
+
+  */
+
+public class _01_Notes {
+}
