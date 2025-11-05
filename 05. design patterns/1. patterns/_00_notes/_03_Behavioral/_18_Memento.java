e99ba99512990a41de480f9e2bfca0ea96c93e31
jarviswuod <johnsonmakings@gmail.com>
2025-11-05 18:36:22 +0300

    Design Pattern minor Update

--- 05. design patterns/1. patterns/_00_notes/_03_Behavioral/_18_Memento.java
+++ 05. design patterns/1. patterns/_00_notes/_03_Behavioral/_18_Memento.java
@@ -5,10 +5,10 @@ package _03_Behavioral;
     NOTES:
     - Memento Pattern:
         - Is a behavioral design pattern that captures and externalizes the internal state of an object without breaking it's encapsulation
-        - It allows you to save and restore state of an object providing the ability to undo or rollback changes, and also save history of the state
+        - It allows you to save and restore state of an object providing the ability to undo or rollback changes and also save history of the state
 
 
-    - 3 Main actors in Memento;
+    - 3 Main actors in Memento:
         a. Originator:
             - Is a class whose state needs to be saved and restored. It creates memento objects to capture it's state and restore it's state from the memento
 
@@ -16,7 +16,7 @@ package _03_Behavioral;
             - Is a class that stores the internal state of the originator
 
         c. Caretaker:
-            - Is a class that stores and manages mementos, it stores and managed mementos. It requests mementos from the originator and can restore the originator state
+            - Is a class that stores and manages mementos. It requests mementos from the originator and can restore the originator state
 
 
     - Advantages and Disadvantages
