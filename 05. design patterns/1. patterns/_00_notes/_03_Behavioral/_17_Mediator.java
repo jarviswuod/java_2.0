f92ea658066af796fe73c88a0c4f27974aaf929b
jarviswuod <johnsonmakings@gmail.com>
2025-11-06 18:21:43 +0300

    Design Pattern minor Update

--- 05. design patterns/1. patterns/_00_notes/_03_Behavioral/_17_Mediator.java
+++ 05. design patterns/1. patterns/_00_notes/_03_Behavioral/_17_Mediator.java
@@ -4,20 +4,21 @@ package _03_Behavioral;
 
     NOTES:
     - Mediator Pattern:
-        - Is a behavioral design pattern that facilitates communication between different objects without them necessarily being aware of each other or being directly coupled
+        - Is a behavioral design pattern that facilitates communication between different objects without them necessarily being aware of each other
         - The interaction is handled by a central point, the mediator
 
 
     - 4 Main actors in Mediator;
         a. Mediator:
-            - Is an interface that defines the communication methods that concrete methods must implement.
+            - Is an interface that defines the communication methods that concrete methods must implement
             - It also maintains references to the specific objects
 
         b. Concrete Mediator class:
             - It implements the mediator interface and manages the interactions and coordination between the participating objects
 
         c. Component:
-            - Is an interface that defines the communication methods that concrete components must implement. It maintains a reference to the mediator
+            - Is an interface that defines the communication methods that concrete components must implement
+            - It maintains a reference to the mediator
 
         d. Concrete Components:
             - Are classes that implement the component interface and communicate with the other components through the mediator
@@ -25,15 +26,15 @@ package _03_Behavioral;
 
     - Advantages and Disadvantages
         - Advantages:
-            - Decoupling and centralized control: It promoted loose coupling and easier maintenance by reducing direct dependencies between components. This centralized control helps manage complex interactions and avoid tangles relationships among components.
-            - Enhanced extensibility: Adding new components or chaining interactions patterns becomes easier with modifications being localized to the mediator enhancing extensibility
+            - Decoupling and centralized control: It promotes loose coupling and easier maintenance by reducing direct dependencies between components. This centralized control helps manage complex interactions and avoid tangled relationships among components
+            - Enhanced extensibility: Adding new components or chaining interaction patterns becomes easier with modifications being localized to the mediator enhancing extensibility
 
 
         - Disadvantages:
             - Complexity: Additional complexity is realized particularly when dealing with many components and intricate interaction
             - Single point of failure: It can become a single point of failure or a performance bottleneck if it become overly complex or tight coupled with the components
             - Limited components Independence: It limits the independence of the components since they need to communicate through the mediator which is only ideal on some scenarios
-            - Performance overhead: Depending on how it;s implemented using mediator might introduce a slight performance overhead due to the additional layer of communication required
+            - Performance overhead: Depending on how it's implemented using mediator might introduce a slight performance overhead due to the additional layer of communication required
 
  */
 
