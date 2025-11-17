9a22353771c0910b7b04fcafcb0f4cadf88289f3
jarviswuod <johnsonmakings@gmail.com>
2025-11-17 06:08:54 +0300

    Design Pattern Revision Season 1

--- java revision/05. design patterns/_03_Design/Behavior/Command/Notes.java
+++ java revision/05. design patterns/_03_Design/Behavior/Command/Notes.java
@@ -1,5 +1,8 @@
 package _03_Design.Behavior.Command;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /*
 
     NOTES:
@@ -113,10 +116,106 @@ package _03_Design.Behavior.Command;
             }
 
 
+    - UNDO PATTERN ::: ::: ::: :::
+
+
  */
 
 public class Notes {
     public static void main(String[] args) {
 
+        HtmlDocument htmlDoc = new HtmlDocument();
+        History history = new History();
+
+        htmlDoc.content = "Hello World";
+        System.out.println(htmlDoc.content); // Hello World
+
+        ItalicCommand italicCommand = new ItalicCommand(htmlDoc, history);
+        italicCommand.execute();
+        System.out.println(htmlDoc.content); // <i>Hello World</i>
+
+        UndoCommand undoCommand = new UndoCommand(history);
+        undoCommand.execute();
+        System.out.println(htmlDoc.content); // Hello World
+    }
+}
+
+class HtmlDocument {
+    public String content;
+
+    public void makeItalic() {
+        content = "<i>" + content + "</i>";
+    }
+}
+
+interface Command {
+
+    void execute();
+}
+
+interface UndoableCommand extends Command {
+
+    void unexecute();
+}
+
+class History {
+
+    private Deque<UndoableCommand> commands = new ArrayDeque<>();
+
+    public void push(UndoableCommand command) {
+        commands.push(command);
+    }
+
+    public UndoableCommand pop() {
+        if (commands.isEmpty()) {
+            return null;
+        }
+        return commands.pop();
+    }
+
+    public int size() {
+        return commands.size();
+    }
+}
+
+class UndoCommand implements Command {
+
+    private History history;
+
+    public UndoCommand(History history) {
+        this.history = history;
+    }
+
+    @Override
+    public void execute() {
+        if (history.size() > 0) {
+
+            UndoableCommand lastCommand = history.pop();
+            lastCommand.unexecute(); // Deegating the undo logic to undoable command object
+        }
+    }
+}
+
+class ItalicCommand implements UndoableCommand {
+
+    private HtmlDocument document;
+    private String prevContent = "";
+    private History history;
+
+    public ItalicCommand(HtmlDocument document, History history) {
+        this.document = document;
+        this.history = history;
+    }
+
+    @Override
+    public void execute() {
+        prevContent = document.content;
+        document.makeItalic(); // delegate work to the doc business object
+        history.push(this);
+    }
+
+    @Override
+    public void unexecute() {
+        document.content = prevContent;
     }
 }
