643faa435a10400218bbeb84721b31805f6bd91e
jarviswuod <johnsonmakings@gmail.com>
2025-11-17 22:25:58 +0300

    Design Pattern Revision Season 1

---
+++ java revision/05. design patterns/_03_Design/Behavior/ChainOfRes/Notes.java
@@ -0,0 +1,147 @@
+package _03_Design.Behavior.ChainOfRes;
+
+/*
+
+    NOTES:
+    - Is a behavioral pattern that enables the chaining of several objects where each object can be choose to processe a request or pass it to the next object on chain
+    - 2 Key players:
+        - Handler interface
+        - Concrete Handler
+
+
+    - CHAIN OF RES::: ::: ::: :::
+
+    . 
+
+
+ */
+
+public class Notes {
+    public static void main(String[] args) {
+
+        Validator validator = new Validator();
+        Authenticator authenticator = new Authenticator();
+        Logger logger = new Logger();
+
+        validator.setNext(authenticator).setNext(logger);
+        WebServer server = new WebServer(validator);
+
+        HttpRequest request = new HttpRequest("admin", "admin@123");
+        server.handle(request);
+
+        HttpRequest request2 = new HttpRequest("admin", "@123");
+        server.handle(request2);
+
+        HttpRequest request3 = new HttpRequest("", "");
+        server.handle(request3);
+    }
+}
+
+class HttpRequest {
+
+    private String username;
+    private String password;
+
+    public String validatedString;
+    public String validatedPassword;
+
+    public HttpRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getValidatedString() {
+        return validatedString;
+    }
+
+    public void setValidatedString(String validatedString) {
+        this.validatedString = validatedString;
+    }
+
+    public String getValidatedPassword() {
+        return validatedPassword;
+    }
+
+    public void setValidatedPassword(String validatedPassword) {
+        this.validatedPassword = validatedPassword;
+    }
+}
+
+class WebServer {
+
+    private Handler handler;
+
+    public WebServer(Handler handler) {
+        this.handler = handler;
+    }
+
+    public void handle(HttpRequest request) {
+        handler.handle(request);
+    }
+}
+
+abstract class Handler {
+
+    private Handler nextHandler;
+
+    public Handler setNext(Handler nextHandler) {
+        this.nextHandler = nextHandler;
+        return nextHandler;
+    }
+
+    public void handle(HttpRequest request) {
+        if (doHandle(request))
+            return;
+
+        if (nextHandler != null)
+            nextHandler.handle(request);
+    }
+
+    public abstract boolean doHandle(HttpRequest request);
+}
+
+class Validator extends Handler {
+
+    @Override
+    public boolean doHandle(HttpRequest request) {
+
+        String username = request.getUsername();
+        String password = request.getPassword();
+
+        request.setValidatedString(username);
+        request.setValidatedPassword(password);
+
+        System.out.println("Validation successful for " + username);
+
+        return request.getValidatedString() == "" || request.getValidatedPassword() == "";
+    }
+}
+
+class Authenticator extends Handler {
+
+    @Override
+    public boolean doHandle(HttpRequest request) {
+        String username = request.getValidatedString();
+        String password = request.getValidatedPassword();
+
+        return !("admin".equals(username) && "admin@123".equals(password));
+    }
+}
+
+class Logger extends Handler {
+
+    @Override
+    public boolean doHandle(HttpRequest request) {
+        System.out.println("Logging request for " + request.getValidatedString());
+
+        return false;
+    }
+}
--- java revision/05. design patterns/_03_Design/Behavior/Command/Notes.java
+++ java revision/05. design patterns/_03_Design/Behavior/Command/Notes.java
@@ -1,8 +1,5 @@
 package _03_Design.Behavior.Command;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /*
 
     NOTES:
@@ -116,106 +113,252 @@ import java.util.Deque;
             }
 
 
+
     - UNDO PATTERN ::: ::: ::: :::
 
+            public class Notes {
+                public static void main(String[] args) {
+
+                    HtmlDocument htmlDoc = new HtmlDocument();
+                    History history = new History();
+
+                    htmlDoc.content = "Hello World";
+                    System.out.println(htmlDoc.content); // Hello World
+
+                    ItalicCommand italicCommand = new ItalicCommand(htmlDoc, history);
+                    italicCommand.execute();
+                    System.out.println(htmlDoc.content); // <i>Hello World</i>
+
+                    UndoCommand undoCommand = new UndoCommand(history);
+                    undoCommand.execute();
+                    System.out.println(htmlDoc.content); // Hello World
+                }
+            }
+
+            class HtmlDocument {
+                public String content;
+
+                public void makeItalic() {
+                    content = "<i>" + content + "</i>";
+                }
+            }
+
+            interface Command {
+
+                void execute();
+            }
+
+            interface UndoableCommand extends Command {
+
+                void unexecute();
+            }
+
+            class History {
+
+                private Deque<UndoableCommand> commands = new ArrayDeque<>();
+
+                public void push(UndoableCommand command) {
+                    commands.push(command);
+                }
+
+                public UndoableCommand pop() {
+                    if (commands.isEmpty()) {
+                        return null;
+                    }
+                    return commands.pop();
+                }
+
+                public int size() {
+                    return commands.size();
+                }
+            }
+
+            class UndoCommand implements Command {
+
+                private History history;
+
+                public UndoCommand(History history) {
+                    this.history = history;
+                }
+
+                @Override
+                public void execute() {
+                    if (history.size() > 0) {
+
+                        UndoableCommand lastCommand = history.pop();
+                        lastCommand.unexecute(); // Delegating the undo logic to undoable command object
+                    }
+                }
+            }
+
+            class ItalicCommand implements UndoableCommand {
+
+                private HtmlDocument document;
+                private String prevContent = "";
+                private History history;
+
+                public ItalicCommand(HtmlDocument document, History history) {
+                    this.document = document;
+                    this.history = history;
+                }
+
+                @Override
+                public void execute() {
+                    prevContent = document.content;
+                    document.makeItalic(); // delegate work to the doc business object
+                    history.push(this);
+                }
+
+                @Override
+                public void unexecute() {
+                    document.content = prevContent;
+                }
+            }
+
+
+        - UNDO::: ::: ::: :::
+
+            public class Notes {
+                public static void main(String[] args) {
+
+                    History history = new History();
+                    HTMLDocument htmlDocument = new HTMLDocument();
+                    ItalicCommand italicCommand = new ItalicCommand(history, htmlDocument);
+                    BoldCommand boldCommand = new BoldCommand(history, htmlDocument);
+
+                    System.out.println(htmlDocument.getContent());
+
+                    italicCommand.execute();
+                    System.out.println(htmlDocument.getContent());
+
+                    boldCommand.execute();
+                    System.out.println(htmlDocument.getContent());
+
+                    UndoCommand undoCommand = new UndoCommand(history);
+                    undoCommand.execute();
+                    System.out.println(htmlDocument.getContent());
+
+                    undoCommand.execute();
+                    System.out.println(htmlDocument.getContent());
+
+                }
+            }
+
+            interface Command {
+
+                void execute();
+            }
+
+            interface UndoableCommand extends Command {
+
+                void unexecute();
+            }
+
+            class History {
+
+                private Deque<UndoableCommand> commands = new ArrayDeque<>();
+
+                public void push(UndoableCommand cmd) {
+                    commands.push(cmd);
+                }
+
+                public UndoableCommand pop() {
+                    if (!commands.isEmpty())
+                        return commands.pop();
+
+                    return null;
+                }
+            }
+
+            class HTMLDocument {
+
+                private String content = "Hello Jarvis";
+
+                public void makeItalic() {
+                    setContent("<i>" + getContent() + "</i>");
+                }
+
+                public String getContent() {
+                    return content;
+
+                }
+
+                public void setContent(String content) {
+                    this.content = content;
+
+                }
+
+                public void makeBold() {
+                    setContent("<b>" + getContent() + "</b>");
+                }
+            }
+
+            class UndoCommand implements Command {
+
+                private History history;
+
+                public UndoCommand(History history) {
+                    this.history = history;
+                }
+
+                @Override
+                public void execute() {
+                    history.pop().unexecute();
+                }
+            }
+
+            class ItalicCommand implements UndoableCommand {
+
+                private History history;
+                private HTMLDocument htmlDocument;
+                private String prevContent = "";
+
+                public ItalicCommand(History history, HTMLDocument htmlDocument) {
+                    this.history = history;
+                    this.htmlDocument = htmlDocument;
+                }
+
+                @Override
+                public void execute() {
+                    prevContent = htmlDocument.getContent();
+                    htmlDocument.makeItalic();
+                    history.push(this);
+                }
+
+                @Override
+                public void unexecute() {
+                    htmlDocument.setContent(prevContent);
+                }
+            }
+
+            class BoldCommand implements UndoableCommand {
+
+                private History history;
+                private HTMLDocument htmlDocument;
+                private String prevContent = "";
+
+                public BoldCommand(History history, HTMLDocument htmlDocument) {
+                    this.history = history;
+                    this.htmlDocument = htmlDocument;
+                }
+
+                @Override
+                public void execute() {
+                    prevContent = htmlDocument.getContent();
+                    htmlDocument.makeBold();
+                    history.push(this);
+                }
+
+                @Override
+                public void unexecute() {
+                    htmlDocument.setContent(prevContent);
+                }
+            }
 
  */
 
 public class Notes {
     public static void main(String[] args) {
 
-        HtmlDocument htmlDoc = new HtmlDocument();
-        History history = new History();
-
-        htmlDoc.content = "Hello World";
-        System.out.println(htmlDoc.content); // Hello World
-
-        ItalicCommand italicCommand = new ItalicCommand(htmlDoc, history);
-        italicCommand.execute();
-        System.out.println(htmlDoc.content); // <i>Hello World</i>
-
-        UndoCommand undoCommand = new UndoCommand(history);
-        undoCommand.execute();
-        System.out.println(htmlDoc.content); // Hello World
-    }
-}
-
-class HtmlDocument {
-    public String content;
-
-    public void makeItalic() {
-        content = "<i>" + content + "</i>";
-    }
-}
-
-interface Command {
-
-    void execute();
-}
-
-interface UndoableCommand extends Command {
-
-    void unexecute();
-}
-
-class History {
-
-    private Deque<UndoableCommand> commands = new ArrayDeque<>();
-
-    public void push(UndoableCommand command) {
-        commands.push(command);
-    }
-
-    public UndoableCommand pop() {
-        if (commands.isEmpty()) {
-            return null;
-        }
-        return commands.pop();
-    }
-
-    public int size() {
-        return commands.size();
-    }
-}
-
-class UndoCommand implements Command {
-
-    private History history;
-
-    public UndoCommand(History history) {
-        this.history = history;
-    }
-
-    @Override
-    public void execute() {
-        if (history.size() > 0) {
-
-            UndoableCommand lastCommand = history.pop();
-            lastCommand.unexecute(); // Deegating the undo logic to undoable command object
-        }
-    }
-}
-
-class ItalicCommand implements UndoableCommand {
-
-    private HtmlDocument document;
-    private String prevContent = "";
-    private History history;
-
-    public ItalicCommand(HtmlDocument document, History history) {
-        this.document = document;
-        this.history = history;
-    }
-
-    @Override
-    public void execute() {
-        prevContent = document.content;
-        document.makeItalic(); // delegate work to the doc business object
-        history.push(this);
-    }
-
-    @Override
-    public void unexecute() {
-        document.content = prevContent;
     }
 }
