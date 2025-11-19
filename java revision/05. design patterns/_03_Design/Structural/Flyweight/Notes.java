96a7a4bcbdd9401498957fd1f7669d3b1f75b524
jarviswuod <johnsonmakings@gmail.com>
2025-11-19 20:16:13 +0300

    Design Pattern Revision Season 1

---
+++ java revision/05. design patterns/_03_Design/Structural/Flyweight/Notes.java
@@ -0,0 +1,116 @@
+package _03_Design.Structural.Flyweight;
+
+/*
+
+    NOTES:
+    - Flyweight design pattern:
+
+    - Is a design pattern that introduces an object to access control of another object allowing for modifications without affecting the original object
+
+    - 3 Key players:
+        - Flyweight Factory
+        - Flyweight class
+        - Object
+
+
+        
+    - FIRST TRIAL::: ::: :::
+
+            public class Notes {
+                public static void main(String[] args) {
+
+                    CropService cropService = new CropService(new CropFactory());
+
+                    for (Crop crop : cropService.getList()) {
+                        crop.render();
+                    }
+                }
+            }
+
+            enum CropType {
+
+                Carrot, Wheat, Maize
+            }
+
+            class Crop {
+
+                private int x;
+                private int y;
+
+                private CropIcon cropIcon;
+
+                public Crop(int x, int y, CropIcon cropIcon) {
+                    this.x = x;
+                    this.y = y;
+                    this.cropIcon = cropIcon;
+                }
+
+                public void render() {
+                    System.out.println("Rendering solution " + cropIcon + " " + x + " " + y);
+                }
+            }
+
+            class CropIcon {
+
+                private CropType cropType;
+                private byte icon[];
+
+                public CropIcon(CropType cropType, byte[] icon) {
+                    this.cropType = cropType;
+                    this.icon = icon;
+                }
+
+                public CropType getCropType() {
+                    return cropType;
+                }
+            }
+
+            class CropService {
+
+                private CropFactory cropFactory;
+
+                public CropService(CropFactory cropFactory) {
+                    this.cropFactory = cropFactory;
+                }
+
+                public List<Crop> getList() {
+                    List<Crop> list = new ArrayList<>();
+
+                    Crop crop1 = new Crop(9, 2, cropFactory.getCropType(CropType.Carrot));
+                    Crop crop2 = new Crop(3, 3, cropFactory.getCropType(CropType.Carrot));
+                    Crop crop3 = new Crop(2, 4, cropFactory.getCropType(CropType.Carrot));
+                    Crop crop4 = new Crop(3, 6, cropFactory.getCropType(CropType.Carrot));
+                    Crop crop5 = new Crop(1, 7, cropFactory.getCropType(CropType.Carrot));
+
+                    list.add(crop1);
+                    list.add(crop2);
+                    list.add(crop3);
+                    list.add(crop4);
+                    list.add(crop5);
+
+                    return list;
+                }
+            }
+
+            class CropFactory {
+
+                private Map<CropType, CropIcon> icons = new HashMap<>();
+
+                public CropIcon getCropType(CropType cropType) {
+
+                    if (!icons.containsKey(cropType)) {
+                        CropIcon cropIcon = new CropIcon(cropType, null);
+                        icons.put(cropType, cropIcon);
+                    }
+                    return icons.get(cropType);
+                }
+            }
+
+
+ */
+
+public class Notes {
+    public static void main(String[] args) {
+
+    }
+}
--- java revision/05. design patterns/_03_Design/Structural/Proxy/Notes.java
+++ java revision/05. design patterns/_03_Design/Structural/Proxy/Notes.java
@@ -3,11 +3,11 @@ package _03_Design.Structural.Proxy;
 /*
 
     NOTES:
-    - 
+    - Is a structural pattern that provides an object to control access to another object allowing for additional functionality without affecting the original object code
     - 3 key players:
-        - 
-        - 
-        - 
+        - Subject interface
+        - Real subject
+        - Proxy
 
 
             public class Notes {
@@ -18,6 +18,7 @@ package _03_Design.Structural.Proxy;
 
                     for (String videoId : videoIds) {
                         videoList.add(new YoutubeVideoProxy(videoId));
+
                         // videoList.add(new YoutubeVideo(videoId));
                     }
 
@@ -95,6 +96,111 @@ package _03_Design.Structural.Proxy;
                 }
             }
 
+
+    -  REVISION::
+        - Proxy design pattern:
+            - Is a structural pattern that provides an object to control access to another object allowing for modifications without affecting original object
+            - 3 Key players:
+                - Subject
+                - Real subject
+                - Proxy
+
+
+                        public class Notes {
+                            public static void main(String[] args) {
+
+                                VideoList videoList = new VideoList();
+                                String[] ids = { "adbde", "fedbq", "abc123", "zbcvt2" };
+
+                                for (String id : ids) {
+                                    // videoList.add(new YoutubeVideo(id));
+
+                                    videoList.add(new YoutubeVideoProxy(id));
+                                }
+
+                                System.out.println();
+
+                                videoList.watch("adbde");
+                                videoList.watch("adbde");
+                            }
+                        }
+
+                        interface Video {
+
+                            String videoId();
+
+                            void render();
+                        }
+
+                        class VideoList {
+
+                            private Map<String, Video> list = new HashMap<>();
+
+                            public void add(Video video) {
+                                list.put(video.videoId(), video);
+                            }
+
+                            public void watch(String videoId) {
+                                Video video = list.get(videoId);
+                                video.render();
+                            }
+                        }
+
+                        class YoutubeVideoProxy implements Video {
+
+                            private String videoId;
+                            private YoutubeVideo youtubeVideo;
+
+                            public YoutubeVideoProxy(String videoId) {
+                                this.videoId = videoId;
+                            }
+
+                            public String getVideoId() {
+                                return videoId;
+                            }
+
+                            @Override
+                            public String videoId() {
+                                return videoId;
+                            }
+
+                            @Override
+                            public void render() {
+                                if (youtubeVideo == null)
+                                    youtubeVideo = new YoutubeVideo(videoId);
+
+                                youtubeVideo.render();
+                            }
+                        }
+
+                        class YoutubeVideo implements Video {
+
+                            String videoId;
+
+                            public YoutubeVideo(String videoId) {
+                                this.videoId = videoId;
+                                download();
+                            }
+
+                            private void download() {
+                                System.out.println("Downloading Video with id of " + videoId);
+                            }
+
+                            public String getVideoId() {
+                                return videoId;
+                            }
+
+                            @Override
+                            public String videoId() {
+                                // System.out.println("IDK");
+                                return videoId;
+                            }
+
+                            @Override
+                            public void render() {
+                                System.out.println("Rendering video with id " + videoId + " to screen");
+                            }
+                        }
 
  */
 
