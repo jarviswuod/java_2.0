550cd73034456548aeced66b10efd99e738a5655
jarviswuod <johnsonmakings@gmail.com>
2025-11-20 21:28:08 +0300

    Design Pattern minor Update

--- 05. design patterns/2. patterns/_04_DesignPatterns/Structural/Facade/Explanation.java
+++ 05. design patterns/2. patterns/_04_DesignPatterns/Structural/Facade/Explanation.java
@@ -5,181 +5,179 @@ package _04_DesignPatterns.Structural.Facade;
     CODE EXPLANATION:
 
     - CHALLENGE:
-        - Say that we have an eCommerce application that allows users to submit orders
-        - Every client that needs to make an order has to go through all the 5 main steps to make a single order. Every client becomes coupled to, or dependent on, four classes; Authenticate, Inventory, Payment, and OrderFulfillment
+        - Say that we have an eCommerce application that allows users to submit orders. Every client that needs to make an order has to go through all the 5 main steps to make a single order. Every client becomes coupled to or dependent on four classes; Authenticate, Inventory, Payment, and OrderFulfillement. That's a lot of coupling
+
 
     - SOLUTION 1:
         - We have some random code to simulate steps involved in processing an order
 
         - OrderRequest class:
-            - This is the request object containing the user-submitted data in a form they filled
-            - The user has a name, cardNumber they entered, amount for the order, address of the user and the itemId's that the user wants to order
+            - This is the request object containing the user-submitted data from a filled form
+            - It contains a name, user's cardNumber, amount for the order, user's address and user's oredered item Id's
 
-                public class OrderRequest {
+                    public class OrderRequest {
 
-                    public String name = "Danny";
-                    public String cardNumber = "1234";
-                    public double amount = 20.99;
-                    public String address = "123 Springfield Way, Texas";
+                        private String name = "Danny";
+                        private String cardNumber = "1234";
+                        private double amount = 20.99;
+                        private String address = "123 Springfield Way, Texas";
 
-                    // item ids user wants to order
-                    public String[] itemIds = { "123", "456", "555", "989" };
-                }
+                        // item ids user wants to order
+                        private String[] itemIds = { "123", "456", "555", "989" };
+                    }
 
 
-        - Authenticate class;
-            - We go through some authenication steps, the class is empty to make things simple but this might check the user's session, checks whether the session is valid and that the username etc
+        - Authenticate class:
+            - We go through some authentication steps
+            - The class is empty to make things simple but this might check the user's session checking if the session is valid
 
-                public class Authenticate {
+                    public class Authenticate {
 
-                }
+                    }
 
 
         - Inventory class:
-            - We have a couple of methods checkInventory() receives itemIds to check the items availablility
-            - reduceInventory() it reduces the inventory of an itemId passed by a certain amount
+            - checkInventory() method receives itemIds and checks for items availablility
+            - reduceInventory() reduces the inventory of an itemId passed by a certain amount
 
-                public class Inventory {
+                    public class Inventory {
 
-                    public boolean checkInventory(String itemId) {
-                        return true; // just return true to keep things simple
+                        public boolean checkInventory(String itemId) {
+                            return true; // just return true to keep things simple
+                        }
+
+                        public void reduceInventory(String itemId, int amount) {
+                            System.out.println("Reducing inventory of " + itemId + " by " + amount);
+                        }
                     }
-
-                    public void reduceInventory(String itemId, int amount) {
-                        System.out.println("Reducing inventory of " + itemId + " by " + amount);
-                    }
-                }
 
 
         - Payment class:
             - This class handles payment step within the order process
-            - Here we can set the name, cardNumber and amount that the user owes for the order
+            - Here we set the name, cardNumber and amount that the user owes for the order
             - We have a method called pay() which simulates connection to some payment service and charging the user's card
 
-                public class Payment {
+                    public class Payment {
 
-                    private String name;
-                    private String cardNumber;
-                    private double amount;
+                        private String name;
+                        private String cardNumber;
+                        private double amount;
 
-                    public Payment(String name, String cardNumber, double amount) {
-                        this.name = name;
-                        this.cardNumber = cardNumber;
-                        this.amount = amount;
+                        public Payment(String name, String cardNumber, double amount) {
+                            this.name = name;
+                            this.cardNumber = cardNumber;
+                            this.amount = amount;
+                        }
+
+                        public void pay() {
+                            System.out.println("Charging card with name " + name);
+                        }
                     }
-
-                    public void pay() {
-                        System.out.println("Charging card with name " + name);
-                    }
-                }
 
 
         - OrderFulfillment class:
-            -  Which checks the inventory object if we have sufficient inventory
-            - We have a fulfill() method which is going to loop through the items  in the items list i.e items that the user is going to user wants to use and it's going to reduce the inventory of those items in the loop
-            - We also have a log to console "inserting the orde into the db"
+            -  Checks inventory object to verify we have sufficient inventory
+            - fulfill() method loops through items in items list i.e items the user is buying and it's reduces the inventory of those items in the loop
+            - We also log to console "inserting order into database"
 
-                public class OrderFulfillment {
+                    public class OrderFulfillment {
 
-                    private Inventory inventory;
+                        private Inventory inventory;
 
-                    public OrderFulfillment(Inventory inventory) {
-                        this.inventory = inventory;
-                    }
+                        public OrderFulfillment(Inventory inventory) {
+                            this.inventory = inventory;
+                        }
 
-                    public void fulfill(String name, String address, String[] items) {
-                        System.out.println("Inserting order into database");
+                        public void fulfill(String name, String address, String[] items) {
+                            System.out.println("Inserting order into database");
 
-                        for (String item : items) {
-                            inventory.reduceInventory(item, 1);
+                            for (String item : items) {
+                                inventory.reduceInventory(item, 1);
+                            }
                         }
                     }
-                }
 
 
 
         - Main class: Client Class
-            - NOTE: - All these are classes that simulate the process involved in the order process
-            - IMPORTANT BIT: how the client interacts with the solution;
-
-            - We create a new orderRequest object
-            - We then create our authenication object
-            - Then we create our Inventory object and over here we will loop through our orderRequest itemIds and then we can check the inventory for each id
-            - Then we create our payment ovject passing information from the OrderRequest object and then we call payments.pay() method
-            - Finally we create our orderFulfillment object where we pass our inventroy object and then we call our fulfill() method passing infor from orderRewuest object
+            - We create a new OrderRequest object
+            - We then create a new Authentication object
+            - Then create Inventory object and over here we will loop through our orderRequest itemIds and then we can check the inventory for each id
+            - Then we create Payment object passing information from the OrderRequest object calling payments.pay() method
+            - Finally, create OrderFulfillment object where we pass our inventroy object calling fulfill() method passing infor from orderRequest object
             - It's all working and running smooth
 
-                public class Main {
-                    public static void main(String[] args) {
+                    public class Main {
+                        public static void main(String[] args) {
 
-                        OrderRequest orderRequest = new OrderRequest();
-                        Authenticate auth = new Authenticate();
+                            OrderRequest orderRequest = new OrderRequest();
+                            Authenticate auth = new Authenticate();
 
-                        System.out.println(auth);
+                            System.out.println(auth);
 
-                        Inventory inventory = new Inventory();
-                        for (String id : orderRequest.itemIds) {
-                            inventory.checkInventory(id);
+                            Inventory inventory = new Inventory();
+                            for (String id : orderRequest.itemIds) {
+                                inventory.checkInventory(id);
+                            }
+
+                            Payment payment = new Payment(orderRequest.name, orderRequest.cardNumber, orderRequest.amount);
+                            payment.pay();
+
+                            OrderFulfillment orderFulfillment = new OrderFulfillment(inventory);
+                            orderFulfillment.fulfill(orderRequest.name, orderRequest.address, orderRequest.itemIds);
                         }
-
-                        Payment payment = new Payment(orderRequest.name, orderRequest.cardNumber, orderRequest.amount);
-                        payment.pay();
-
-                        OrderFulfillment orderFulfillment = new OrderFulfillment(inventory);
-                        orderFulfillment.fulfill(orderRequest.name, orderRequest.address, orderRequest.itemIds);
                     }
-                }
 
 
 
     - SOLUTION 2: FACADE PATTERN;
-        - We maintain every single class we had from Authenticate, Inventory, Payment, and OrderFulfillment. We only introduce an OrderSerice class to act as an abstraction
+        - We maintain every single class we had from Authenticate, Inventory, Payment to OrderFulfillment. We only introduce an OrderService class to act as an abstraction
 
         - OrderService class
-            - This class acta as Facade that encapulates all the complexities and steps involved in making an order so clients can then make orders without having to know all the details or steps involved in the order process
+            - This class acts as a Facade that encapsulates all complexities and steps involved in making an order so clients can then make orders without having to know all the details or steps involved in the order process
 
-            - We have a single method called order() it takes in OrderRequest and then over here we'll have all the steps involved in maknig an order - this will be excactly the same as what we have under our Main class currently
+            - We have order() method which takes in OrderRequest and then over here we'll have all the steps involved in making an order. This will be exactly the same as what we have under our Main class currently
 
-                public class OrderService {
+                    public class OrderService {
 
-                    public void order(OrderRequest orderReq) {
+                        public void order(OrderRequest orderReq) {
 
-                        OrderRequest orderRequest = new OrderRequest();
-                        Authenticate auth = new Authenticate();
+                            OrderRequest orderRequest = new OrderRequest();
+                            Authenticate auth = new Authenticate();
 
-                        System.out.println(auth);
+                            System.out.println(auth);
 
-                        Inventory inventory = new Inventory();
-                        for (String id : orderRequest.itemIds) {
-                            inventory.checkInventory(id);
+                            Inventory inventory = new Inventory();
+                            for (String id : orderRequest.itemIds) {
+                                inventory.checkInventory(id);
+                            }
+
+                            Payment payment = new Payment(orderRequest.name, orderRequest.cardNumber, orderRequest.amount);
+                            payment.pay();
+
+                            OrderFulfillment orderFulfillment = new OrderFulfillment(inventory);
+                            orderFulfillment.fulfill(orderRequest.name, orderRequest.address, orderRequest.itemIds);
                         }
-
-                        Payment payment = new Payment(orderRequest.name, orderRequest.cardNumber, orderRequest.amount);
-                        payment.pay();
-
-                        OrderFulfillment orderFulfillment = new OrderFulfillment(inventory);
-                        orderFulfillment.fulfill(orderRequest.name, orderRequest.address, orderRequest.itemIds);
                     }
-                }
 
 
 
         - Main class: Client class;
-            - All we need here is the orderRequest object
-            - We then create an orderService object and then call the order() method passing in the orderRequest object we just created above
+            - All we need here is the OrderRequest object
+            - We then create an OrderService object and then call the order() method passing in the orderRequest object we just created above
 
-                public class Main {
-                    public static void main(String[] args) {
+                    public class Main {
+                        public static void main(String[] args) {
 
-                        OrderRequest orderRequest = new OrderRequest();
-                        OrderService orderService = new OrderService();
-                        orderService.order(orderRequest);
+                            OrderRequest orderRequest = new OrderRequest();
+                            OrderService orderService = new OrderService();
+                            orderService.order(orderRequest);
+                        }
                     }
-                }
 
 
         - OBSERVATION:
-            - Code works excactly the same as before. Now clients can make order without having to depend on many classes and without having to know about all the complexities involved in making an order, all the complexities are abstracted away and encapulated within the orderService class
+            - Code works excactly the same as before. Now clients can make order without having to depend on many classes and without having to know about all the complexities involved in making an order, all the complexities are abstracted away and encapulated within the OrderService class
 
  */
 
--- 05. design patterns/2. patterns/_04_DesignPatterns/Structural/Facade/Notes.java
+++ 05. design patterns/2. patterns/_04_DesignPatterns/Structural/Facade/Notes.java
@@ -4,65 +4,69 @@ package _04_DesignPatterns.Structural.Facade;
 
     NOTES:
     - The Facade Pattern:
-        - Is a structural design pattern that provides a simplified interface to a complex system, encapsulating the complexities of multiple subsystems into a single unified interface for clients
+        - Is a structural design pattern that provides a simplified interface to a complex system to help encapsulate it's complexities and improve the usability
+
 
     - CHALLENGE:
         - Say that we have an eCommerce application that allows users to submit orders
 
-                                        ______________________
-                       User submits     | Server             |
-                         orders         |____________________|
-            USER    ------------------->| Authenticate       |
-                                        | check Inventory    |
-                                        | Connect to payment |
-                                        | fulfill orders     |
-                                        | charge Users       |
-                ^                       |____________________|
-                |        Response:               |
-                |     Success or failure         |
-                |________________________________|
+                                            ______________________
+                           User submits     | Server             |
+                             orders         |____________________|
+                USER    ------------------->| Authenticate       |
+                                            | Check Inventory    |
+                  ^                         | Connect to payment |
+                  |                         | Fulfill orders     |
+                  |                         | Charge Users       |
+                  |                         |____________________|
+                  |          Response:                |
+                  |     Success or failure            |
+                  |___________________________________|
 
 
-        - As you can see, every client that needs to make an order has to go through all the 5 main steps to make a single order. Every client becomes coupled to, or dependent on, four classes; Authenticate, Inventory, Payment, and OrderFulfillement
-
-        - If we had ten classes that needed to make an order, then we'd have ten classes dependent on these four clases - that's a lot of coupling. If one of these four classes changes, that's ten classes that may need updating. Not good
 
         - Each client that needs to make an order will be dependent on four classes;
+                                         _________________
+                                         |    Client     |
+                                         |_______________|
+                                                 |
+                                                 |
+                       __________________________|_____________________________
+                       |                  |                |                  |
+                       |                  |                |                  |
+                       v                  v                v                  v
+                ________________   _______________   ______________   ____________________
+                | Authenticate |   | Inventory   |   | Payment    |   | OrderFulfillment |
+                |______________|   |_____________|   |____________|   |__________________|
 
-                                        _________________
-                                        |  Client       |
-                                        |_______________|
-                                                |
-                                                |
-                    ____________________________|__________________________________
-                    |                     |                   |                   |
-                    |                     |                   |                   |
-                    v                     v                   v                   v
-                _________________  __________________   ______________   ____________________
-                | Authenticate  |  | Inventory      |   | Payment    |   | OrderFulfillment |
-                |_______________|  |________________|   |____________|   |__________________|
 
+
+        - As you can see, every client that needs to make an order has to go through all the 5 main steps to make a single order. Every client becomes coupled to or dependent on four classes; Authenticate, Inventory, Payment, and OrderFulfillement
+
+        - If we had ten classes that needed to make an order, then we'd have ten classes dependent on these four clases. That's a lot of coupling. If one of these four classes changes then ten classes that may need updating
 
 
 
     - FACADE PATTERN SOLUTION:
-        - Clients making an order don't care and don't need to know about the steps involved in making an order; they just want to make an order
-        - So, let's introduce a new class, OrderService, with a single method, order(), that abstracts all the logic, so all other classes that need to make order only have to depend on this one class
+        - Clients making an order don't care or need to know about the steps involved in making an order. They just want to make an order
+        - So, let's introduce OrderService class having only order() method that abstracts all the logic so all other classes that need to make order only have to depend on this one class
 
-                                        _______________________
-                                        | OrderService        |
-                                        |_____________________|
-                                        | Order(orderRequest) |
-                                        |_____________________|
-                                                |
-                                                |
-                    ____________________________|__________________________________
-                    |                     |                   |                   |
-                    |                     |                   |                   |
-                    v                     v                   v                   v
-                _________________  __________________   ______________   ____________________
-                | Authenticate  |  | Inventory      |   | Payment    |   | OrderFulfillment |
-                |_______________|  |________________|   |____________|   |__________________|
+                                       _______________________
+                                       |    OrderService     |
+                                       |_____________________|
+                                       | Order(orderRequest) |
+                                       |_____________________|
+                                                 |
+                                                 |
+                       __________________________|_____________________________
+                       |                  |                |                  |
+                       |                  |                |                  |
+                       v                  v                v                  v
+                ________________   _______________   ______________   ____________________
+                | Authenticate |   | Inventory   |   | Payment    |   | OrderFulfillment |
+                |______________|   |_____________|   |____________|   |__________________|
+
+
 
 
         - Now, all classes that need to be able to make an order only need to depend on the one OrderService class:
