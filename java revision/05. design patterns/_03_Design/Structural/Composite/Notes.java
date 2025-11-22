aed98e3cdbde73de605ea0569d5a6209c5a1ef07
jarviswuod <johnsonmakings@gmail.com>
2025-11-22 17:02:29 +0300

    Design Pattern Revision Season 1

---
+++ java revision/05. design patterns/_03_Design/Structural/Composite/Notes.java
@@ -0,0 +1,22 @@
+package _03_Design.Structural.Composite;
+
+/*
+
+    Notes:
+    - Is a structural pattern that uses a tree-like structure to represent a collection of objects where individual objects and groups of objects are treated in a unified manner
+
+    - 3 key players:
+        - Component
+        - Leaf
+        - Composite
+
+    - Example:
+        - Amazon delivery shopping package:
+            - 
+
+
+*/
+
+public class Notes {
+
+}
