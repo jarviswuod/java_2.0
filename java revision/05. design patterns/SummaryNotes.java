f2ea20b0855d199a4ccc802d7dbee7b12f20aab5
jarviswuod <johnsonmakings@gmail.com>
2025-11-25 21:33:03 +0300

    Design Pattern Revision Season 1

---
+++ java revision/05. design patterns/SummaryNotes.java
@@ -0,0 +1,253 @@
+/*
+
+    NOTES:
+    - Behavioral design pattern:
+        - Visitor
+        - Template Method
+        - Strategy
+        - State
+        - Observer
+        - Mediator
+        - Memento
+        - Iterator
+        - Interpreter
+        - Command
+        - Chain of responsibility
+
+
+    - Structural design pattern:
+        - Proxy
+        - Flyweight
+        - Facade
+        - Decorator
+        - Composite
+        - Bridge
+        - Adapter
+
+
+    // -----------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+
+
+    - Visitor pattern:
+        - Is a behavioral pattern that separates an algorithm from an object on which it operates on
+        - 4 Key players:
+            - Visitor interface
+            - Concrete Visitor
+            - Element interface
+            - Concrete Element
+
+        - Example:
+            - 
+
+
+    - Template method pattern:
+        - Is a behavioral pattern provides a skeleton of an algorithm for detailed implementation by subclasses extending the algos super class
+        - 2 Key players:
+            - Abstract class
+            - Concrete classes
+
+        - Example:
+            - 
+
+
+    - Strategy pattern:
+        - Is a behavioral pattern that defines a family of related algorithms that can be swapped at runtime by an object
+        - 3 Key players:
+            - Context
+            - Strategy interface
+            - Concrete Strategy
+
+        - Example:
+            - 
+
+
+    - State pattern:
+        - Is a behavioral pattern that allows an object to change it's behavior when it's internal state changes
+        - 3 Key players:
+            - Context
+            - State interface
+            - Concrete State
+
+        - Example:
+            - 
+
+
+    - Observer pattern:
+        - Is a behavioral pattern that defines a one to many relationship between objects and changes in one object is automatically communicated and reflected in other objects
+        - 3 Key players:
+            - publisher
+            - subscriber
+            - Concrete subscriber
+
+        - Example:
+            - 
+
+
+    - Memento pattern:
+        - Is a behavioral pattern that enables us capture and externalizes the internals of an object without affecting the objects encapsulation
+        - 3 Key players:
+            - Originator
+            - memento
+            - Caretaker
+
+        - Example:
+            - 
+
+
+    - Mediator pattern:
+        - Is a behavioral pattern that provides a communication mechanism when objects via a bridge without the objects necessasirly knowing of each other existence
+        - 4 Key players:
+            - Mediator interface
+            - Concrete Mediator
+            - Component interface
+            - Concrete Component
+
+        - Example:
+            - 
+
+
+    - Iterator pattern:
+        - Is a behavioral pattern that provides a mechanism for iterating over objects without exposing the object internal structure plus changing the internals doesnot break the client code
+        - 4 Key players:
+            - Iterator interface
+            - Concrete Iterator
+            - Collection interface
+            - Concrete Collection
+
+        - Example:
+            - 
+
+
+    - Interpreter pattern:
+        - Is a behavioral pattern that provides a mechanism for interprets language, formula or expression
+        - 0 Key players:
+            - 
+
+        - Example:
+            -
+
+
+    - Command pattern:
+        - Is a behavioral pattern that converts request to an object containing every information about the request
+        - 4 Key players:
+            - Command interface
+            - Concrete Command
+            - Invoker
+            - Receiver
+
+        - Example:
+            - 
+
+
+    - Chain of Responsibility pattern:
+        - Is a behavioral pattern that allows you to create a chain of objects where each object can either handle a request or pass it along to the next object
+        - 2 Key players:
+            - Handler interface
+            - Concrete Handler
+
+        - Example:
+            - 
+
+    // -----------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+
+
+
+    - Proxy pattern:
+        - Is a structural pattern that
+        - 4 Key players:
+            - Visitor interface
+            - Concrete Visitor
+            - Element interface
+            - Concrete Element
+
+        - Example:
+            - 
+
+
+    - Flyweight pattern:
+        - Is a structural pattern that
+        - 4 Key players:
+            - Visitor interface
+            - Concrete Visitor
+            - Element interface
+            - Concrete Element
+
+        - Example:
+            - 
+
+
+    - Facade pattern:
+        - Is a structural pattern that
+        - 4 Key players:
+            - Visitor interface
+            - Concrete Visitor
+            - Element interface
+            - Concrete Element
+
+        - Example:
+            - 
+
+
+    - Decorator pattern:
+        - Is a structural pattern that
+        - 4 Key players:
+            - Visitor interface
+            - Concrete Visitor
+            - Element interface
+            - Concrete Element
+
+        - Example:
+            - 
+
+
+    - Composite pattern:
+        - Is a structural pattern that
+        - 4 Key players:
+            - Visitor interface
+            - Concrete Visitor
+            - Element interface
+            - Concrete Element
+
+        - Example:
+            - 
+
+
+    - Bridge pattern:
+        - Is a structural pattern that
+        - 4 Key players:
+            - Visitor interface
+            - Concrete Visitor
+            - Element interface
+            - Concrete Element
+
+        - Example:
+            - 
+
+
+    - Adapter pattern:
+        - Is a structural pattern that
+        - 4 Key players:
+            - Visitor interface
+            - Concrete Visitor
+            - Element interface
+            - Concrete Element
+
+        - Example:
+            - 
+
+
+
+
+*/
+
+public class SummaryNotes {
+
+}
