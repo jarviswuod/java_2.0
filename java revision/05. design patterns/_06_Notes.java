bce685b222de47d89f801a6e07c7f97269ec364c
jarviswuod <johnsonmakings@gmail.com>
2025-12-20 23:46:13 +0300

    Design Pattern Revision Season 1

---
+++ java revision/05. design patterns/_06_Notes.java
@@ -0,0 +1,29 @@
+/*
+
+    NOTES:
+    - Behavioral
+        - 
+
+    - Structural
+        - 
+        - Bridge
+        - Command
+        - Chain
+        - Flyweight
+        - Facade
+        - Proxy
+
+    - Creational
+        - FactoryMethod
+        - AbstractFactory
+        - Singleton
+        - Prototype
+        - Builder 
+
+
+
+*/
+
+public class _06_Notes {
+
+}
