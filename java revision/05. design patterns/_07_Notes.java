efb0fb6093271c15ecb27d189caf5133ec01d257
jarviswuod <johnsonmakings@gmail.com>
2026-01-08 14:04:54 +0300

    Design Pattern Revision Season 1

---
+++ java revision/05. design patterns/_07_Notes.java
@@ -0,0 +1,276 @@
+/*
+
+    NOTES:
+
+    - OOP concepts:
+        - Abstraction
+        - Encapsulation
+        - Inheritance
+        - Polymorphism
+        - Composition
+        - Coupling
+
+    - SOLID principle:
+        - Single responsibility
+        - Open closed principle
+        - Liskov substitution
+        - Interface Segregation
+        - Dependency Injection
+
+
+    - Design pattern:
+        - Creational:
+            - Singleton:
+                - 
+                - Key players:
+                    - 
+                    - 
+                    - 
+                    - 
+
+                - Example:
+                    - 
+
+            - Prototype:
+                - 
+                - Key players:
+                    - 
+                    - 
+                    - 
+                    - 
+
+                - Example:
+                    - 
+
+            - Builder:
+                - 
+                - Key players:
+                    - 
+                    - 
+                    - 
+                    - 
+
+                - Example:
+                    - 
+
+            - Factory Method:
+                - 
+                - Key players:
+                    - 
+                    - 
+                    - 
+                    - 
+
+                - Example:
+                    - 
+
+            - Abstract Factory:
+                - 
+                - Key players:
+                    - 
+                    - 
+                    - 
+                    - 
+
+                - Example:
+                    - 
+
+
+
+        - Structural:
+            - Proxy:
+                - Adding an object to access another object
+                - Key players:
+                    - Subject
+                    - Real subject
+                    - Proxy
+
+                - Example:
+                    - 
+
+            - Facade:
+                - 
+                - Key players:
+                    - 
+                    - 
+                    - 
+                    - 
+
+                - Example:
+                    - 
+
+            - Decorator:
+                - 
+                - Key players:
+                    - Decorator interface
+                    - Concrete decorator
+                    - Component interface
+                    - Concrete component
+
+                - Example:
+                    - 
+
+            - Flyweight:
+                - Share common internal object state to help save memory space
+                - Key players:
+                    - Flyweight Factory
+                    - Flyweight Object
+                    - 
+                    - 
+
+                - Example:
+                    - 
+
+            - Composite:
+                - A collection presentation of objects in a tree-like structure where an individual object and a collection of objects are treated equal
+                - Key players:
+                    - Component
+                    - Leaf
+                    - Composite
+
+                - Example:
+                    - 
+
+            - Bridge:
+                - 
+                - Key players:
+                    - 
+                    - 
+                    - 
+                    - 
+
+                - Example:
+                    - 
+
+            - Adapter:
+                - 
+                - Key players:
+                    - Adaptor
+                    - Adaptee
+*                    - 
+
+                - Example:
+                    - 
+
+
+
+        - Behavioral:
+            - Visitor:
+                - 
+                - Key players:
+                    - Visitor interface
+                    - Concrete visitor
+                    - Component interface
+                    - Concrete component
+
+                - Example:
+                    - 
+
+            - Template Method:
+                - Skeleton of an algorithm with specifics provided at concrete level
+                - Key players:
+*                    - Interface method
+*                    - Method implementation
+
+                - Example:
+                    - 
+
+            - Strategy:
+                - A family of related algos that can be swapped at run time
+                - Key players:
+                    - Strategy interface
+                    - Concrete strategy
+                    - Context
+
+                - Example:
+                    - 
+
+            - State:
+                - Changing objects behavior based on internal object state changes
+                - Key players:
+                    - State interface
+                    - Concrete states
+                    - Context
+
+                - Example:
+                    - 
+
+            - Observer:
+                - Changes in one object is reflected and communicated to other objects
+                - Key players:
+                    - Publisher
+                    - Observer
+                    - Objects 
+
+                - Example:
+                    - 
+
+            - Memento:
+                - Capturing and exporting internal state of an object without affecting encapsulation
+                - Key players:
+                    - Originator
+                    - Memento
+                    - History 
+
+                - Example:
+                    - 
+
+            - Mediator:
+                - Using an object as a communcation mechanism between different objects
+                - Key players:
+                    - Mediator interface
+                    - Concrete Mediator
+                    - Component interface
+                    - Concrete component
+
+                - Example:
+                    - 
+
+            - Iterator:
+                - Iteratoring over a collection of objects
+                - Key players:
+                    - Iterator interface
+                    - Concrete iterator
+                    - Collection interface
+                    - Concrete collection
+
+                - Example:
+                    - 
+
+            - Interpreter:
+                - Interpreting symbols, formulas and languages
+                - Key players:
+                    - 
+
+                - Example:
+                    - 
+
+            - Chain Of Responsilibity:
+                - A collection of object that can either process a request of pass it down to the next object to process it
+                - Key players:
+                    - Handler interface
+                    - Concrete handler
+                    - 
+
+                - Example:
+                    - 
+
+            - Command:
+                - Converting requests to objects with every information about the request
+                - Key players:
+                    - 
+                    - 
+                    - 
+                    - 
+
+                - Example:
+                    - 
+                    - 
+
+*/
+
+public class _07_Notes {
+    public static void main(String[] args) {
+        System.out.println("Another banger!!!!");
+    }
+}
