daffd4cf790dc350540d7b2fe7877e8aa12a73d6
jarviswuod <johnsonmakings@gmail.com>
2026-01-10 23:56:41 +0300

    Design Pattern Revision Season 1

---
+++ java revision/05. design patterns/_08_Notes.java
@@ -0,0 +1,20 @@
+/*
+
+    NOTES:
+    - OOP:
+        - Abstractional
+        - Encapsulation
+        - 
+        - 
+        - 
+
+    - Design patterns
+        - Creational pattern
+        - Behavioral pattern
+        - Structural pattern
+
+*/
+
+public class _08_Notes {
+
+}
